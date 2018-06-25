258ba5429890e63ce9bd035a6869ab2fd7dc9cb6
bdb <bhuiya@github.com>
2018-06-24 23:13:53 -0400

    Cohort layout

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,10 +1,8 @@
 class Student < ApplicationRecord
+  has_many :students_cohorts, dependent: :destroy
+  has_many :cohorts, through: :students_cohorts
 
-    has_many :students_cohorts, dependent: :destroy
-    has_many :cohorts, through: :students_cohorts
-
-    def full_name
-      "#{first_name} #{last_name}"
-    end
-
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
